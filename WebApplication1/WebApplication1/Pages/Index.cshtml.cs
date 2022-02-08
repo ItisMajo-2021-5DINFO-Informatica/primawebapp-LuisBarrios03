92ac152d2b956d18c7606486d788bd5a709d6ae9
Luis Andrè Barrios Luna Victoria <91853345+LuisBarrios03@users.noreply.github.com>
2022-02-08 14:16:40 +0100

    Query
    Aggiunta del data Base Norhtwind con la query di comando

--- WebApplication1/WebApplication1/Pages/Index.cshtml.cs
+++ WebApplication1/WebApplication1/Pages/Index.cshtml.cs
@@ -1,16 +1,23 @@
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Data.SqlClient;
+using SqlCommand = Microsoft.Data.SqlClient.SqlCommand;
+using SqlConnection = Microsoft.Data.SqlClient.SqlConnection;
+using SqlDataReader = Microsoft.Data.SqlClient.SqlDataReader;
+using SqlException = Microsoft.Data.SqlClient.SqlException;
 
 namespace WebApplication1.Pages
 {
     public class IndexModel : PageModel
     {
+
         public void OnGet()
         {
-        }
+        }       
     }
 }
